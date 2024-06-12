5d49632296a5cefd71ff5efee0e27736db2748db
mts88 <fsca.motisi@gmail.com>
2024-06-13 00:19:58 +0200

    chore: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const DEFAULT_PORT = 3000;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CalistheniX App')
+    .setDescription('The API of the CalistheniX App')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+
   await app.listen(DEFAULT_PORT);
 }
 bootstrap();
